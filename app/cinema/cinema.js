284d3d01f71ca6b211a165f9cce1259363fb486a
Anas <lakchouchanas@gmail.com>
2021-12-24 20:46:01 +0100

    add: scrollable containers

--- app/cinema/cinema.js
+++ app/cinema/cinema.js
@@ -9,6 +9,16 @@ angular.module('myApp.cinema', ['ngRoute'])
   });
 }])
 
-.controller('CinemaCtrl', [function() {
+.controller('CinemaCtrl', ["$scope", function($scope) {
+    let movieContainer = angular.element(document.querySelector(".movies-container"))[0];
+    let calendarContainer = angular.element(document.querySelector(".calendar-container"))[0];
+    $scope.calendarScrollLeft = function ($event) {
+        $event.preventDefault();
+        calendarContainer.scrollLeft += $event.deltaY;
+    }
 
+    $scope.movieScrollLeft = function ($event) {
+        $event.preventDefault();
+        movieContainer.scrollLeft += $event.deltaY;
+    }
 }]);
