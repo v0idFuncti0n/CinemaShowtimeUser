9d9fda6a116503f500bdb045e3320ad8b40c6d74
Anas <lakchouchanas@gmail.com>
2021-12-28 19:46:08 +0100

    add: some improvements

--- app/cinema/cinema.js
+++ app/cinema/cinema.js
@@ -12,6 +12,7 @@ angular.module('myApp.cinema', ['ngRoute'])
 .controller('CinemaCtrl', ["$scope","$http", function($scope,$http) {
     let movieContainer = angular.element(document.querySelector(".movies-container"))[0];
     let calendarContainer = angular.element(document.querySelector(".calendar-container"))[0];
+
     $scope.calendarScrollLeft = function ($event) {
         $event.preventDefault();
         calendarContainer.scrollLeft += $event.deltaY;
@@ -26,7 +27,7 @@ angular.module('myApp.cinema', ['ngRoute'])
     //to get Movies
     $scope.allMovies = [];
 
-    $scope.getDays = function (){
+    $scope.getDays = function () {
         var tempDate = [];
         tempDate.push(new Date());
         for (var i = 1; i <= 14; i++) {
@@ -37,25 +38,30 @@ angular.module('myApp.cinema', ['ngRoute'])
     };
     $scope.getDays();
 
-
-
     $scope.test= function (date){
         var sentDate = new Date(date);
         var dateString = sentDate.getFullYear() + "-" + (sentDate.getMonth() + 1) +"-" + sentDate.getDate();
         // use dateString to send request from the API ( get movieByDate)
-        $http.get("http://localhost:8080/api/movies/byDate/"+ dateString)
+        $http.get("http://localhost:8080/api/movies/byDate/" + dateString)
             .then(function success(response){
                 $scope.allMovies = response.data;
                 console.log(response.data);
-
             },function error(response){
                 console.log(response);
             });
 
-
         console.log(dateString);
     };
 
+    $scope.test($scope.todayDate[0]);
 
-
+    $scope.activate = function ($event) {
+        let dayItems = angular.element(document.querySelector(".active"));
+        for (let i = 0; i < dayItems.length; i++) {
+            // Remove the class 'active' if it exists
+            dayItems[i].classList.remove('active');
+        }
+        $event.currentTarget.classList.add("active");
+        console.log($event.currentTarget);
+    }
 }]);
